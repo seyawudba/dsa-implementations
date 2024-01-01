1196f7db23a895bf7361db7006bf42f2dc7eb6c5
ybaseyram <seyawudb@gmail.com>
2024-01-01 20:06:58 +0000

    Initialize a linkedlist

--- linkedlist.py
+++ linkedlist.py
@@ -3,3 +3,19 @@ class Node:
         "Initializing a node class with a value and the next field set to none."
         self.value = value
         self.next = None
+    
+    def get_value(self):
+        return self.value
+    
+    def set_value(self,new_value):
+        self.value = new_value
+
+    def __repr__(self):
+        return f"{self.value}"
+    
+
+class LinkedList:
+    node = Node(None)
+    def __init__(self,head=None):
+        """A linkedlist can be defined empty in the initial stage"""
+        self.head = head
