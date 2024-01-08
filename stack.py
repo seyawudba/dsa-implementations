058f29fc35de22bf32fbe9830b3a0249a7811b50
ybaseyram <seyawudb@gmail.com>
2024-01-08 10:58:47 +0000

    Add a peek method

--- stack.py
+++ stack.py
@@ -2,8 +2,14 @@ class Stack:
     def __init__(self):
         self.top = None
         self.stack = []
+
     def push(self,value):
         self.stack.append(value)
         self.top = self.stack[-1]
+
+    def peek(self):
+        if not self.isempty:
+            return self.top
+        return "List is empty."
     
     
