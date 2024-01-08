13598a550eaa5f029aee0a544528cbe5ef21ff13
ybaseyram <seyawudb@gmail.com>
2024-01-08 10:56:25 +0000

    A push method to add values to the stack

--- stack.py
+++ stack.py
@@ -1,16 +1,9 @@
-#push
-#pop
-#isEmpty
-#isFull
-#peek
-#size
 class Stack:
     def __init__(self):
         self.top = None
         self.stack = []
-
     def push(self,value):
-        i = 0
-        self.top = self.stack.append(value)
-        i +=0
+        self.stack.append(value)
+        self.top = self.stack[-1]
+    
     
