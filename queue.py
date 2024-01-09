d3c2767ba0ecffeccfc31671314627d768f1a9c0
ybaseyram <seyawudb@gmail.com>
2024-01-09 17:20:19 +0000

    Add the addnew, remove and check list status.

--- queue.py
+++ queue.py
@@ -3,3 +3,33 @@ class Queue:
         self.first = None
         self.last = None
         self.queue = []
+
+    def addnew(self,value):
+        self.queue.append(value)
+        if len(self.queue) == 1:
+            self.first = self.last = value
+        else:
+            self.last = self.queue[-1]
+
+        self.last = self.queue[-1]
+
+    def removefrom(self):
+        status = self.isempty()
+        if status:
+            return "The queue is empty"
+        
+        popped_value = self.queue.pop(0)
+        if not status:
+            self.first = self.queue[0]
+        else:
+            return status
+        return popped_value
+        
+
+    
+
+    def isempty(self):
+        status = bool(self.queue)
+        if status:
+            return not status
+        return status
